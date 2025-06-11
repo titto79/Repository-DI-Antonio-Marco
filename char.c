4e1b9f7c4e2263e976e8efcc1f648357a7df62fc
titto79 <36604740+titto79@users.noreply.github.com>
2025-06-11 10:52:08 +0200

    Create char.c

---
+++ char.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>   // per isupper, tolower
+
+int main(void) {
+    char c;
+    printf("Inserisci un carattere: ");
+    c = getchar();             // legge un singolo carattere da stdin
+    // ISUPPER->è minuscolo?
+    if (isupper((unsigned char)c)) {
+        // se è una lettera maiuscola, la converto in minuscola
+        //TOLOWER-> converte in minuscolo 
+        char lower = tolower((unsigned char)c);
+        printf("Hai inserito una maiuscola. In minuscolo diventa: %c\n", lower);
+    } else if (islower((unsigned char)c)) {
+        // se è una lettera minuscola, la converto in maiuscola
+        //TOUPPER-> converte in magliuscolo
+        char upper = toupper((unsigned char)c);
+        printf("Hai inserito una minuscola. In maiuscolo diventa: %c\n", upper);
+    } else {
+        printf("Il carattere '%c' non è una lettera alfabetica.\n", c);
+    }
+
+    return 0;
+}
